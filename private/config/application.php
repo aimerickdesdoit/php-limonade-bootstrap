3c99b36eaef5ac8de889c0e39c5a20de4610465e
Aimerick Desdoit <aimerick@lunaweb.fr>
2015-06-02 15:13:40 +0200

    Correction display_errors

--- private/config/application.php
+++ private/config/application.php
@@ -7,7 +7,6 @@ define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
 
 ini_set('error_reporting',            E_ALL | E_STRICT);
 ini_set('display_startup_errors',     0);
-ini_set('display_errors',             APPLICATION_ENV == 'development' ? 1 : 0);
 ini_set('short_open_tag',             0);
 ini_set('default_charset',            'UTF-8');
 ini_set('mbstring.internal_encoding', 'UTF-8');
@@ -18,9 +17,10 @@ ini_set('magic_quotes_sybase',        0);
 ini_set('magic_quotes_runtime',       0);
 ini_set('auto_detect_line_endings',   0);
 
-define('PRIVATE_DIR',     dirname(dirname(__FILE__)));
+define('PRIVATE_DIR', dirname(dirname(__FILE__)));
 
 require_once PRIVATE_DIR . '/lib/vendor/autoload.php';
+ini_set('display_errors', APPLICATION_ENV == 'development' ? 1 : 0);
 
 function configure() {  
   option('base_uri',      '/orsane/public/');

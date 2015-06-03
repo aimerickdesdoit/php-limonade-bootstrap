d9239d2a852523464455d2d1d20ff2f902860403
Aimerick Desdoit <aimerick@lunaweb.fr>
2015-06-03 15:34:42 +0200

    Ajout des fonctions new_record et findBy sur les modèles

--- lib/model.php
+++ lib/model.php
@@ -4,21 +4,43 @@ class Model {
 
   protected $_attributes = array();
   protected $_errors = array();
+  protected static $_validators = array();
 
   public function __construct($attributes = array()) {
     $this->_attributes = $attributes;
   }
 
   public function __get($var) {
-      return isset($this->_attributes[$var]) ? $this->_attributes[$var] : null;
+    return isset($this->_attributes[$var]) ? $this->_attributes[$var] : null;
   }
 
   public function __set($var, $value) {
-      $this->_attributes[$var] = $value;
+    $this->_attributes[$var] = $value;
   }
 
   public function getError($attribute) {
     return isset($this->_errors[$attribute]) ? $this->_errors[$attribute] : null;
+  }
+
+  public function new_record() {
+    return is_null($this->id);
+  }
+
+  public function validate() {
+    foreach (static::$_validators as $field => $validators) {
+      $validators = (array) $validators;
+      foreach ($validators as $validator) {
+        $method = '_validate' . $validator;
+        $this->$method($field);
+      }
+    }
+    return count($this->_errors) == 0;
+  }
+
+  protected function _validatePresence($field) {
+    if (!$this->$field) {
+      $this->_errors[$field] = 'Ce champ est obligatoire';
+    }
   }
 
   public function save($attributes = array()) {
@@ -83,24 +105,68 @@ class Model {
     //   ['id' => '', 'label' => ''],
     //   ['id' => '', 'label' => '']
     // ]
-    $results = $stmt->fetchAll();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $objects = array();
+    $records = array();
     foreach ($results as $result) {
-      // $objects[] = new Model($result);
-      array_push($objects, new static($result));
+      // $records[] = new Model($result);
+      array_push($records, new static($result));
     }
 
-    return $objects;
+    return $records;
   }
 
   public static function find($id) {
     $stmt = self::pdo()->prepare('SELECT * FROM '.static::$_table_name.' WHERE id = :id LIMIT 1;');
     $stmt->execute(array('id' => $id));
-    $results = $stmt->fetchAll();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    $record = new static(array_shift($results));
+    if (!$result) {
+      halt(NOT_FOUND, 'Record not found');
+    }
+
+    $record = new static($result);
     return $record;
+  }
+
+  public static function findAllBy($attribute, $value) {
+    $stmt = self::pdo()->prepare('SELECT * FROM '.static::$_table_name.' WHERE '.self::_quoteIdentifier($attribute).' = :value LIMIT 1;');
+    $stmt->execute(array('value' => $value));
+    // [
+    //   ['id' => '', 'label' => ''],
+    //   ['id' => '', 'label' => ''],
+    //   ['id' => '', 'label' => '']
+    // ]
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $records = array();
+    foreach ($results as $result) {
+      // $records[] = new Model($result);
+      array_push($records, new static($result));
+    }
+
+    return $records;
+  }
+
+  public static function findBy($attribute, $value, $halt = false) {
+    $stmt = self::pdo()->prepare('SELECT * FROM '.static::$_table_name.' WHERE '.self::_quoteIdentifier($attribute).' = :value LIMIT 1;');
+    $stmt->execute(array('value' => $value));
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($halt && !$result) {
+      halt(NOT_FOUND, 'Record not found');
+    }
+
+    if (!$result) {
+      return null;
+    }
+
+    $record = new static($result);
+    return $record;
+  }
+
+  protected static function _quoteIdentifier($value){
+    return '`'.$value.'`';
   }
 
   public static function pdo() {
